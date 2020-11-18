b8254a9f74a79a1ff133d579d525b6008658ddb6
Erylenn <70634470+Ery-EDEN@users.noreply.github.com>
2020-11-18 11:48:51 +0100

    Add files via upload
    First commit

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+if($_GET['page'] == 'catalogue') {
+    require_once('catalogue.html');
+} else if ($_GET['page'] == 'home') {
+    require_once('home.html');
+} else if ($_GET['page'] == 'reservation') {
+    require_once('reservation.html');
+} else if ($_GET['pay'] == 'pay') {
+    require_once('pay.html');
+}
+?>
